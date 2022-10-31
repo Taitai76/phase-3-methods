e83003d3a2a28d3868d6f88fc5bf30ccdaaf1dad
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-10-31 12:43:48 -0400

    commit tree 885e4a50076c66c200e07417d1ad55e2a513992a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(n)
+    puts "Hello, #{n}!"
+end
+
+def greet_with_default(n = "programmer")
+    puts "Hello, #{n}!"
+end
+
+def add(x, y)
+    x + y
+end
+
+def halve(x)
+    if x.class != Integer
+        return nil
+    end
+     x/2
+end
+
